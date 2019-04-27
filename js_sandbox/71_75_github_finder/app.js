663a711222c9532ee85719e3b459178a3f062d72
maximkoshelenko <qazwsx741852>
2019-04-27 16:26:35 +0300

    Something

--- js_sandbox/71_75_github_finder/app.js
+++ js_sandbox/71_75_github_finder/app.js
@@ -20,6 +20,7 @@ searchUser.addEventListener('keyup', (e) => {
                     ui.showAlert('User Not Found', 'alert alert-danger')
                 } else {
                     ui.showProfile(data.profile);
+                    ui.showRepos(data.repos);
                 }
             });
     } else {
