a69d95133a3fd96bca856eef82d6f0cba38a6318
maximkoshelenko <qazwsx741852>
2019-05-21 22:20:06 +0300

    Factory pattern

---
+++ js_sandbox/94_factory_pattern/app.js
@@ -0,0 +1,48 @@
+function MemberFactory() {
+    this.createMember = function (name, type) {
+        let member;
+
+        if (type === 'simple') {
+            member = new SimpleMembership(name);
+        } else if (type === 'standard') {
+            member = new StandardMembership(name);
+        } else if (type === 'super') {
+            member = new SuperMembership(name);
+        }
+
+        member.type = type;
+        member.define = function () {
+            console.log(`${this.name} (${this.type}): ${this.cost}`);
+        }
+
+        return member;
+    }
+}
+
+const SimpleMembership = function (name) {
+    this.name = name;
+    this.cost = '$5';
+}
+
+const StandardMembership = function (name) {
+    this.name = name;
+    this.cost = '$10';
+}
+
+const SuperMembership = function (name) {
+    this.name = name;
+    this.cost = '$20';
+}
+
+const members = [];
+const factory = new MemberFactory();
+
+members.push(factory.createMember('Maksim', 'super'));
+members.push(factory.createMember('Olka', 'standard'));
+members.push(factory.createMember('Bill', 'simple'));
+
+// console.log(members);
+
+members.forEach(function (member) {
+    member.define();
+});
