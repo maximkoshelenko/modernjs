17e31f8e0cb193ad60c0c3e29561ddcc3958d6f0
maximkoshelenko <qazwsx741852>
2019-05-26 17:54:57 +0300

    State pattern

---
+++ js_sandbox/96_mediator_pattern/appES6.js
@@ -0,0 +1,53 @@
+class User {
+    constructor(name) {
+        this.name = name;
+        this.chatroom = null;
+    }
+
+    send(message, to) {
+        this.chatroom.send(message, this, to)
+    }
+
+    receive(message, from) {
+        console.log(`${from.name} to ${this.name}: ${message}`);
+    }
+}
+
+const Chatroom = function () {
+    let users = {}; // list of users
+
+    return {
+        register: function (user) {
+            users[user.name] = user;
+            user.chatroom = this;
+        },
+        
+        send: function (message, from, to) {
+            if (to) {
+                // Single user message
+                to.receive(message, from);
+            } else {
+                // Mass message
+                for (const key in users) {
+                    if (users[key] !== from) {
+                        users[key].receive(message, from);
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+const maks = new User('Maks');
+const yena = new User('Yena');
+const evreychik = new User('Evreychik');
+
+const chatroom = new Chatroom();
+
+chatroom.register(maks);
+chatroom.register(yena);
+chatroom.register(evreychik);
+
+maks.send('Hello Yena', yena);
+yena.send('Gav Gav');
