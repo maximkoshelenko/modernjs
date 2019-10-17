db182003023deff0b0e82e24d8fd6bb3708e4f00
Maksim Koshelenko <maxim.koshelenko@gmail.com>
2019-10-17 23:02:32 +0300

    Done shit

--- js_sandbox/111_119_micropost/babel_webpack_starter/src/app.js
+++ js_sandbox/111_119_micropost/babel_webpack_starter/src/app.js
@@ -7,6 +7,12 @@ document.addEventListener('DOMContentLoaded', getPosts);
 // Listen for add post
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+// Listen for delete
+document.querySelector('#posts').addEventListener('click', deletePost);
+
+// Listen for delete
+document.querySelector('#posts').addEventListener('click', enableEdit);
+
 // Get posts
 function getPosts() {
     http.get('http://localhost:3000/posts')
@@ -14,7 +20,7 @@ function getPosts() {
         .catch(err => console.log(err));
 }
 
-// Add post
+// Submit post
 function submitPost() {
     const title = document.querySelector('#title').value;
     const body = document.querySelector('#body').value;
@@ -27,9 +33,25 @@ function submitPost() {
     // Create post
     http.post('http://localhost:3000/posts', data)
         .then(data => {
-            // ui.showAlert('Post Added', 'alert alert-success');
+            ui.showAlert('Post Added', 'alert alert-success');
             ui.clearFields();
             getPosts();
         })
         .catch(err => console.log(err));
+}
+
+// Delete Post
+function deletePost(e) {
+    if (e.target.parentElement.classList.contains('delete')) {
+        const id = e.target.parentElement.dataset.id;
+        if(confirm('Are you sure?')) {
+            http.delete(`http://localhost:3000/posts/${id}`)
+            .then(data => {
+                ui.showAlert('Post Removed', 'alert alert-success');
+                getPosts();
+            })
+            .catch(err => console.log(err));
+        }
+    }
+    e.preventDefault();
 }
--- js_sandbox/111_119_micropost/babel_webpack_starter/src/ui.js
+++ js_sandbox/111_119_micropost/babel_webpack_starter/src/ui.js
@@ -28,14 +28,37 @@ class UI {
             </div>
             `;
         });
-        this.post.innerHTML = output; 
+        this.post.innerHTML = output;
     }
 
-    // showAlert(message, className){
-    //     this.clearAlert();
+    showAlert(message, className) {
+        this.clearAlert();
 
+        // Create div
+        const div = document.createElement('div');
+        // Add classes
+        div.className = className;
+        // Add text
+        div.appendChild(document.createTextNode(message));
+        // Get parent
+        const container = document.querySelector('.postsContainer');
+        // Get posts
+        const posts = document.querySelector('#posts');
+        // Insert alert div
+        container.insertBefore(div, posts);
 
-    // }
+        // Timeout
+        setTimeout(() => {
+            this.clearAlert();
+        }, 3000)
+    }
+
+    clearAlert() {
+        const currentAlert = document.querySelector('.alert');
+        if (currentAlert) {
+            currentAlert.remove();
+        }
+    }
 
     clearFields() {
         this.titleInput.value = '';
