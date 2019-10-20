da8712e60caf8868bd18c032fdb5b574b53faedf
Maksim Koshelenko <maxim.koshelenko@gmail.com>
2019-10-20 21:53:22 +0300

    Add profinal steps

--- js_sandbox/111_119_micropost/babel_webpack_starter/src/ui.js
+++ js_sandbox/111_119_micropost/babel_webpack_starter/src/ui.js
@@ -70,10 +70,49 @@ class UI {
     }
 
     // Fill form to edit
-    fillForm(data){
+    fillForm(data) {
         this.titleInput.value = data.title;
         this.bodyInput.value = data.body;
         this.idInput.value = data.id;
+
+        this.changeFormState('edit');
+    }
+
+    // Clear Id hidden value
+    clearIdInput(){
+        this.idInput.value = '';
+    }
+
+    // Change Form State
+    changeFormState(type) {
+        if (type === 'edit') {
+            this.postSubmit.textContent = 'Update Post';
+            this.postSubmit.className = 'post-submit btn btn-warning btn-block';
+
+            // Create cancel button
+            const button = document.createElement('button');
+            button.className = 'post-cancel btn btn-light btn-block';
+            button.appendChild(document.createTextNode('Cancel Edit'));
+
+            // Get parent
+            const cardForm = document.querySelector('.card-form');
+            // Get element to insert before
+            const formEnd = document.querySelector('.form-end');
+            // Insert cancel button
+            cardForm.insertBefore(button, formEnd);
+        } else {
+            this.postSubmit.textContent = 'Post It';
+            this.postSubmit.className = 'post-submit btn btn-primary btn-block';
+
+            // Remove cancel button if it there
+            if (document.queryCommandValue('.post-cancel')) {
+                document.queryCommandValue('.post-cancel').remove();
+            }
+            // Clear ID from hidden field
+            this.clearIdInput();
+            // Clear text
+            this.clearFields();
+        }
     }
 }
 
